55fc0f43f75dad1092ebc3b4bcbec484ce61e953
lyx-debug <lxlyx6212@outlook.com>
2025-08-27 13:50:15 +0800

    fix bug(1)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // 平滑滚动到锚点
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const href = this.getAttribute('href');
+        if (href.startsWith('#')) {
+            e.preventDefault();
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
     });
 });
 
